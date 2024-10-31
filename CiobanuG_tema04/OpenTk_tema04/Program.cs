13be4dd74198a321496e1a7891ebb5c070932c2f
Ciobanu Gabriel <161735200+gabrielC46@users.noreply.github.com>
2024-10-31 19:20:00 +0200

    Update

---
+++ CiobanuG_tema04/OpenTk_tema04/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTk_tema04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Window3D wnd = new Window3D();
+            wnd.Run(30.0, 0.0);
+        }
+    }
+}
